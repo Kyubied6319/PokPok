f56f0149d9e085a069db1966c7d2a39a48c39756
Kyubied6319 <43020429+Kyubied6319@users.noreply.github.com>
2023-10-30 15:02:01 +1100

    Beginnings of Cyclical Farming game

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+const user = {
+  name : 'Tumblepoke',
+  imageUrl: 'https://drive.google.com/uc?export=view&id=1X0ENC_-RHRpF4WkV8ncSUpTcK_aVULwH/view?usp=drive_link',
+  imageSize: 90,
+};
 function App() {
   return (
     <div className="App">
+      <h1>{user.name}</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={user.imageUrl} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and we did here is v2 and now some music.
+          Tumblepoke: "Pok Pok"<br></br> Here is one of its favorite songs
         </p>
         <a
           className="App-link"
